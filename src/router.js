6cd9e2d766ea997c223d64b6773088861b2037e0
KrzychuK121 <krzychuk1211@gmail.com>
2024-08-09 18:58:16 +0200

    Creating base for TrainingManager frontend.

---
+++ src/router.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import App from './components/App';
+import Welcome from './components/Welcome';
+
+export const router = createBrowserRouter(
+    [
+        {
+            path: '/', element: <App />
+        },
+        {
+            path: '/welcome', element: <Welcome />
+        }
+    ]
+);
