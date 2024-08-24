f13536750ebf81066c0f7de299aa644664af42ff
Komputer Domowy <KrzychuK1211@gmail.com>
2024-08-24 15:06:03 +0200

    Changing all " to '.

--- src/routes/Welcome.jsx
+++ src/routes/Welcome.jsx
@@ -5,7 +5,7 @@ import classes from './Welcome.module.css';
 function Welcome() {
     return (
         <Container fluid id={classes.welcomeContainer}>
-            <header className="row justify-content-center align-items-center fixed-top h-25">
+            <header className='row justify-content-center align-items-center fixed-top h-25'>
                 <Col
                     id={classes.nag}
                     md={6}
@@ -14,14 +14,14 @@ function Welcome() {
                     TrainingManager
                 </Col>
             </header>
-            <Row className="justify-content-center align-items-center fixed-bottom h-75">
+            <Row className='justify-content-center align-items-center fixed-bottom h-75'>
                 <Col md={6}>
                     <div id={classes.welcomeDesc}>
                         <p>
                             Dzięki tej aplikacji możesz stworzyć swój wymarzony trening oraz kontrolować jego przebieg.
                         </p>
                         <p>
-                            Kliknij <a href="/glowna">tutaj</a> aby przejść do strony głównej.
+                            Kliknij <a href='/glowna'>tutaj</a> aby przejść do strony głównej.
                         </p>
                     </div>
                 </Col>
