bcdd12bc93c8d37d69e7153e8970684566ad62e2
Komputer Domowy <KrzychuK1211@gmail.com>
2024-08-24 15:07:23 +0200

    Changing all " to '.

--- src/components/main_page/AuthNavigations.jsx
+++ src/components/main_page/AuthNavigations.jsx
@@ -1,18 +1,18 @@
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav } from 'react-bootstrap';
 
 function AuthNavigations({classes}){
-    const name = "admin";
-    const surname = "admin";
+    const name = 'admin';
+    const surname = 'admin';
 
     return (
         <>
             <Navbar.Text
-                className="text-capitalize"
+                className='text-capitalize'
             >
                 Witaj {name} {surname}!
             </Navbar.Text>
             <Nav.Link
-                href="@{/logout}"
+                href='@{/logout}'
                 className={classes.menuLink}
             >
                 Wyloguj się
