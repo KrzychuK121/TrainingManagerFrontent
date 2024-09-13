ab48a3b63cbbe4499ac153f5917345ab5176bce0
Krzysztof <krzychuk1211@gmail.com>
2024-09-13 10:33:39 +0200

    Small changes.

--- src/router.js
+++ src/router.js
@@ -3,40 +3,41 @@ import { createBrowserRouter } from 'react-router-dom';
 import Default from './routes/Default';
 import Welcome from './routes/Welcome';
 import MainPage from './routes/main/MainPage';
-import NonAuthWelcome from "./routes/main/NonAuthWelcome";
-import LoginPage from './routes/navigations/authentication/Login';
+import MainWelcome from "./routes/main/MainWelcome";
+import LoginPage, { action as loginAction } from './routes/navigations/authentication/Login';
 import RegisterPage, { action as registerAction } from './routes/navigations/authentication/Register';
 import LogoutPage from './routes/navigations/authentication/Logout';
 
 export const router = createBrowserRouter(
     [
         {
-            path: '/', element: <Default />, children: [
+            path: '/', element: <Default/>, children: [
                 {
                     path: '/',
                     index: true,
-                    element: <Welcome />
+                    element: <Welcome/>
                 },
                 {
                     path: 'main',
-                    element: <MainPage />,
+                    element: <MainPage/>,
                     children: [
                         {
                             index: true,
-                            element: <NonAuthWelcome />
+                            element: <MainWelcome/>
                         },
                         {
                             path: 'login',
-                            element: <LoginPage />
+                            element: <LoginPage/>,
+                            action: loginAction
                         },
                         {
                             path: 'register',
-                            element: <RegisterPage />,
+                            element: <RegisterPage/>,
                             action: registerAction
                         },
                         {
                             path: 'logout',
-                            element: <LogoutPage />
+                            element: <LogoutPage/>
                         }
                     ]
                 }
