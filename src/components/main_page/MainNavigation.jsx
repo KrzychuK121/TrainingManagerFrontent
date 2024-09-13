0d73677c28ccb3b970d8d6d4a53ede4aa8e78d19
Krzysztof <krzychuk1211@gmail.com>
2024-09-13 10:35:18 +0200

    Using implemented auth function responsible for deciding if user is authenticated or not.

--- src/components/main_page/MainNavigation.jsx
+++ src/components/main_page/MainNavigation.jsx
@@ -1,20 +1,18 @@
-import { useState } from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import classes from './MainNavigation.module.css';
 
 import AuthNavigations from './AuthNavigations';
 import NonAuthNavigations from './NonAuthNavigations';
+import { isAuthenticated } from "../../utils/AuthUtils";
 
 function MainNavigation() {
-    const [authorized, setAuthorized] = useState(false);
-
     return (
         <Container as='header' fluid id={classes.menuContainer}>
             <nav id='menu'>
                 <Navbar expand='lg' data-bs-theme='dark'>
                     <Navbar.Brand>
                         <article id={classes.brandDesc} className={classes.menuContainerComponent}>
-                            <img src='/img/icon-scaled.png' alt='icon.png' style={{ paddingRight: '20px' }} />
+                            <img src='/img/icon-scaled.png' alt='icon.png' style={{paddingRight: '20px'}}/>
                             Trening
                         </article>
                     </Navbar.Brand>
@@ -28,27 +26,27 @@ function MainNavigation() {
                                 Zacznij trening!
                             </Nav.Link>
                             <NavDropdown
-                              title='Zarządzaj zestawami'
-                              menuVariant='dark'
-                              id={classes.menuLinkDropdown}
+                                title='Zarządzaj zestawami'
+                                menuVariant='dark'
+                                id={classes.menuLinkDropdown}
                             >
                                 <NavDropdown.ItemText>Treningi</NavDropdown.ItemText>
                                 <NavDropdown.Item href='@{/training}'>Wyświetl</NavDropdown.Item>
                                 <NavDropdown.Item href='@{/training/create}'>Stwórz</NavDropdown.Item>
-                                <NavDropdown.Divider />
+                                <NavDropdown.Divider/>
                                 <NavDropdown.ItemText>Ćwiczenia</NavDropdown.ItemText>
                                 <NavDropdown.Item href='@{/exercise}'>Wyświetl</NavDropdown.Item>
                                 <NavDropdown.Item href='@{/exercise/create}'>Stwórz</NavDropdown.Item>
-                                <NavDropdown.Divider />
+                                <NavDropdown.Divider/>
                                 <NavDropdown.ItemText>Plany treningowe</NavDropdown.ItemText>
                                 <NavDropdown.Item href='@{/plans}'>Wyświetl wszystkie</NavDropdown.Item>
                                 <NavDropdown.Item href='@{/plans/week}'>Wyświetl aktywny</NavDropdown.Item>
                                 <NavDropdown.Item href='@{/plans/week/create}'>Stwórz</NavDropdown.Item>
                             </NavDropdown>
                             {
-                                authorized
-                                ? <AuthNavigations classes={classes} />
-                                : <NonAuthNavigations classes={classes} />
+                                isAuthenticated()
+                                    ? <AuthNavigations classes={classes}/>
+                                    : <NonAuthNavigations classes={classes}/>
                             }
                         </Nav>
                     </Navbar.Collapse>
