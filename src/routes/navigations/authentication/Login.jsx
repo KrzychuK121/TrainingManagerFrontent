b5cefd504afa37a50ff4e65d552255175698d901
Krzysztof <krzychuk1211@gmail.com>
2024-09-13 10:33:18 +0200

    Providing action for login form.

--- src/routes/navigations/authentication/Login.jsx
+++ src/routes/navigations/authentication/Login.jsx
@@ -1,29 +1,52 @@
-import { Form as RouterForm } from 'react-router-dom';
-import { Form, Col, Row, Button } from 'react-bootstrap';
+import { Button, Col, Form, Row } from 'react-bootstrap';
+import { Form as RouterForm, redirect } from 'react-router-dom';
+import { login } from '../../../utils/AuthUtils';
 import defaultClasses from '../../Default.module.css';
 
-function LoginPage(){
+function LoginPage() {
     return (
-        <Row className='justify-content-center'>
+        <Row className="justify-content-center">
             <Col sm={5}>
-                <RouterForm method='POST'>
+                <RouterForm method="POST">
                     <fieldset className={defaultClasses.authForms}>
                         <legend>Logowanie</legend>
                         <Form.Group>
-                            <Form.Label className='form-label' htmlFor='username'>Login:</Form.Label>
-                            <Form.Control type='text' name='username' className='form-control' id='username'/>
+                            <Form.Label
+                                className="form-label"
+                                htmlFor="username"
+                                column={true}
+                            >
+                                Login:
+                            </Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="username"
+                                className="form-control"
+                                id="username"
+                            />
                         </Form.Group>
                         <Form.Group>
-                            <Form.Label className='form-label' htmlFor='password'>Hasło:</Form.Label>
-                            <Form.Control type='password' name='password' className='form-control' id='password'/>
+                            <Form.Label
+                                className="form-label"
+                                htmlFor="password"
+                                column={true}
+                            >
+                                Hasło:
+                            </Form.Label>
+                            <Form.Control
+                                type="password"
+                                name="password"
+                                className="form-control"
+                                id="password"
+                            />
                         </Form.Group>
-                        <Button variant='primary' type='submit'>
+                        <Button variant="primary" type="submit">
                             Zaloguj
                         </Button>
                         <Form.Check
-                            id='remember-me'
-                            name='remember-me'
-                            label='Zapamiętaj mnie'
+                            id="remember-me"
+                            name="remember-me"
+                            label="Zapamiętaj mnie"
                         />
                     </fieldset>
                 </RouterForm>
@@ -33,3 +56,15 @@ function LoginPage(){
 }
 
 export default LoginPage;
+
+export async function action({request}) {
+    const formData = await request.formData();
+
+    const userCredentials = {
+        'username': formData.get('username'),
+        'password': formData.get('password')
+    };
+    await login(userCredentials);
+
+    return redirect('/main');
+}
