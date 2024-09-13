ce6a6d34c1cd932c1ae1bfcc9bb14a939c8e4982
Krzysztof <krzychuk1211@gmail.com>
2024-09-13 13:50:43 +0200

    Displaying login success information.

--- src/routes/main/MainWelcome.jsx
+++ src/routes/main/MainWelcome.jsx
@@ -1,10 +1,22 @@
 import './MainWelcome.module.css';
+import { Alert } from 'react-bootstrap';
+import { useSearchParams } from 'react-router-dom';
 
 function MainWelcome() {
     // TODO: Maybe change content depending on auth status
+    const [searchParams] = useSearchParams();
+    const loginSuccess = searchParams.get('login-success');
+
     return (
         <>
             <div>
+                {
+                    loginSuccess !== null && (
+                        <Alert variant="success">
+                            Logowanie ukończone pomyślnie.
+                        </Alert>
+                    )
+                }
                 <h1>Witaj w aplikacji!</h1>
                 <p style={{fontSize: '20px'}}>
                     Cieszę się, że zdecydowałeś/aś się skorzystać z aplikacji.
