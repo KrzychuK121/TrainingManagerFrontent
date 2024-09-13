a1dc4050bfbe77d2939591f9ba75c708455d7e2c
Krzysztof <krzychuk1211@gmail.com>
2024-09-13 13:47:38 +0200

    Securing login and register paths for logged users only. Providing logout functionalities. Returning login errors to display.

--- src/utils/AuthUtils.jsx
+++ src/utils/AuthUtils.jsx
@@ -1,7 +1,13 @@
+import { json, redirect } from 'react-router-dom';
+
 const TOKEN_KEY = 'token';
 
 function saveToken(token) {
     localStorage.setItem(TOKEN_KEY, token);
+}
+
+function clearToken() {
+    localStorage.removeItem(TOKEN_KEY);
 }
 
 function getToken() {
@@ -16,6 +22,17 @@ export function isAuthenticated() {
     return getToken() !== null;
 }
 
+/**
+ * This function should be used as loader in paths where
+ * users should be prevented from accessing page after authentication
+ * (e.g.login page).
+ */
+export function authenticatedLoader() {
+    return isAuthenticated() ?
+        redirect('/main')
+        : null;
+}
+
 export async function login(userCredentials) {
     const response = await fetch(
         'http://localhost:8080/api/auth/login',
@@ -28,17 +45,38 @@ export async function login(userCredentials) {
         }
     );
 
-    // Wrong login or password
+    // Wrong password
     if (response.status === 401) {
-        return;
+        return json({error: 'Hasło jest nieprawidłowe.'});
     }
 
     // User does not exist
     if (response.status === 404) {
-        return;
+        return json({error: `Użytkownik ${userCredentials.username} nie istnieje.`});
     }
 
     const data = await response.json();
     const token = data.token;
     saveToken(token);
+
+    return null;
+}
+
+export async function logout() {
+    if (!isAuthenticated())
+        return json({error: 'Nie można wylogować. Użytkownik niezalogowany.'});
+
+    await fetch(
+        'http://localhost:8080/api/auth/logout',
+        {
+            method: 'POST',
+            headers: injectToken({
+                'Content-Type': 'application/json'
+            })
+        }
+    );
+
+    clearToken();
+
+    return null;
 }
