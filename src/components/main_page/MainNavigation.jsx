c5fb957ca6b56ab39901e113286e1650586e5e71
Krzysztof <krzychuk1211@gmail.com>
2024-09-13 13:54:58 +0200

    Using ' instead of ".

--- src/components/main_page/MainNavigation.jsx
+++ src/components/main_page/MainNavigation.jsx
@@ -8,41 +8,41 @@ import NonAuthNavigations from './NonAuthNavigations';
 function MainNavigation() {
     const isAuthenticated = useRouteLoaderData('root');
     return (
-        <Container as="header" fluid id={classes.menuContainer}>
-            <nav id="menu">
-                <Navbar expand="lg" data-bs-theme="dark">
+        <Container as='header' fluid id={classes.menuContainer}>
+            <nav id='menu'>
+                <Navbar expand='lg' data-bs-theme='dark'>
                     <Navbar.Brand>
                         <article id={classes.brandDesc} className={classes.menuContainerComponent}>
-                            <img src="/img/icon-scaled.png" alt="icon.png" style={{paddingRight: '20px'}}/>
+                            <img src='/img/icon-scaled.png' alt='icon.png' style={{paddingRight: '20px'}}/>
                             <span>Trening</span>
                         </article>
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbar-links"/>
+                    <Navbar.Toggle aria-controls='navbar-links'/>
                     <Navbar.Collapse
-                        id="navbar-links"
-                        className="justify-content-center"
+                        id='navbar-links'
+                        className='justify-content-center'
                     >
                         <Nav className={classes.menuContainerComponent}>
-                            <Nav.Link href="/training" className={classes.menuLink}>
+                            <Nav.Link href='/training' className={classes.menuLink}>
                                 Zacznij trening!
                             </Nav.Link>
                             <NavDropdown
-                                title="Zarządzaj zestawami"
-                                menuVariant="dark"
+                                title='Zarządzaj zestawami'
+                                menuVariant='dark'
                                 id={classes.menuLinkDropdown}
                             >
                                 <NavDropdown.ItemText>Treningi</NavDropdown.ItemText>
-                                <NavDropdown.Item href="@{/training}">Wyświetl</NavDropdown.Item>
-                                <NavDropdown.Item href="@{/training/create}">Stwórz</NavDropdown.Item>
+                                <NavDropdown.Item href='@{/training}'>Wyświetl</NavDropdown.Item>
+                                <NavDropdown.Item href='@{/training/create}'>Stwórz</NavDropdown.Item>
                                 <NavDropdown.Divider/>
                                 <NavDropdown.ItemText>Ćwiczenia</NavDropdown.ItemText>
-                                <NavDropdown.Item href="@{/exercise}">Wyświetl</NavDropdown.Item>
-                                <NavDropdown.Item href="@{/exercise/create}">Stwórz</NavDropdown.Item>
+                                <NavDropdown.Item href='@{/exercise}'>Wyświetl</NavDropdown.Item>
+                                <NavDropdown.Item href='@{/exercise/create}'>Stwórz</NavDropdown.Item>
                                 <NavDropdown.Divider/>
                                 <NavDropdown.ItemText>Plany treningowe</NavDropdown.ItemText>
-                                <NavDropdown.Item href="@{/plans}">Wyświetl wszystkie</NavDropdown.Item>
-                                <NavDropdown.Item href="@{/plans/week}">Wyświetl aktywny</NavDropdown.Item>
-                                <NavDropdown.Item href="@{/plans/week/create}">Stwórz</NavDropdown.Item>
+                                <NavDropdown.Item href='@{/plans}'>Wyświetl wszystkie</NavDropdown.Item>
+                                <NavDropdown.Item href='@{/plans/week}'>Wyświetl aktywny</NavDropdown.Item>
+                                <NavDropdown.Item href='@{/plans/week/create}'>Stwórz</NavDropdown.Item>
                             </NavDropdown>
                             {
                                 isAuthenticated
