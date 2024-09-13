4fcdaebb49d103c2e83b3d91fbd46a1e5a0da169
Krzysztof <krzychuk1211@gmail.com>
2024-09-13 13:49:18 +0200

    Creating logout action.

--- src/routes/navigations/authentication/Logout.jsx
+++ src/routes/navigations/authentication/Logout.jsx
@@ -1,18 +1,19 @@
-import { Link } from 'react-router-dom';
 import { Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { logout } from '../../../utils/AuthUtils';
 
 // TODO: Might be deleted later
 
-function LogoutPage(){
+function LogoutPage() {
     return (
         <div style={{color: 'wheat'}}>
             <h1>Nastąpiło wylogowanie z konta</h1>
             <Row
-                className='align-items-center'
+                className="align-items-center"
                 style={{height: '150px'}}
             >
                 <p style={{fontSize: '25px'}}>
-                    Powróć na stronę główną klikając <Link to='/'>tutaj</Link>.
+                    Powróć na stronę główną klikając <Link to="/">tutaj</Link>.
                 </p>
             </Row>
         </div>
@@ -20,3 +21,7 @@ function LogoutPage(){
 }
 
 export default LogoutPage;
+
+export async function action() {
+    return await logout();
+}
