8bde3a2cc03ea0dc77e54738260b822db1c21a20
Krzysztof <krzychuk1211@gmail.com>
2024-09-14 14:03:51 +0200

    Implementing register action. Displaying validation info. Informing user about submitting the form (preventing user from submitting form twice).

--- src/routes/navigations/authentication/Register.jsx
+++ src/routes/navigations/authentication/Register.jsx
@@ -1,17 +1,42 @@
-import { Form as RouterForm, useActionData } from 'react-router-dom';
-import { Button, Col, Row } from 'react-bootstrap';
-
-import defaultClasses from '../../Default.module.css';
+import { Alert, Button, Col, Row, Spinner } from 'react-bootstrap';
+import { Form as RouterForm, redirect, useActionData, useNavigation } from 'react-router-dom';
 import InputField from '../../../components/form/InputField';
 
+import defaultClasses from '../../Default.module.css';
 
-function Register(){
-    const validations = useActionData();
+function getErrorAlert(message) {
+    return <Alert key={message} variant='danger'>{message}</Alert>;
+}
 
-    function getValidation(valProp){
-        if(validations === undefined)
-            return undefined;
-        if(validations === null)
+function getValidations(actionData) {
+    if (actionData === undefined)
+        return undefined;
+
+    return actionData.hasOwnProperty('error')
+        ? undefined
+        : actionData;
+}
+
+function getGlobalError(actionData) {
+    if (actionData === undefined)
+        return undefined;
+
+    if (actionData.hasOwnProperty('status') && actionData.status === 500)
+        return getErrorAlert('Wystąpił błąd serwera.');
+
+    return actionData.hasOwnProperty('error')
+        ? getErrorAlert('Użytkownik już istnieje. Może to Ty?')
+        : null;
+}
+
+function Register() {
+    const navigation = useNavigation();
+    const actionData = useActionData();
+    const validations = getValidations(actionData);
+    const globalError = getGlobalError(actionData);
+
+    function getValidation(valProp) {
+        if (validations === null || validations === undefined)
             return undefined;
 
         return validations.hasOwnProperty(valProp)
@@ -22,6 +47,7 @@ function Register(){
     return (
         <Row className='justify-content-center'>
             <Col sm={5}>
+                {globalError}
                 <RouterForm noValidate validated='true' method='POST'>
                     <fieldset className={defaultClasses.authForms}>
                         <legend>Rejestracja</legend>
@@ -56,7 +82,15 @@ function Register(){
                             status={getValidation('lastName')}
                             name='lastName'
                         />
-                        <Button type='submit' >Zarejestruj się</Button>
+                        {
+                            navigation.state !== 'submitting'
+                                ? <Button type='submit'>Zarejestruj się</Button>
+                                : <Button type='submit' disabled>
+                                    Rejestruję.. {' '}
+                                    <Spinner animation='grow' size='sm'/>
+                                </Button>
+                        }
+
                     </fieldset>
                 </RouterForm>
             </Col>
@@ -66,8 +100,30 @@ function Register(){
 
 export default Register;
 
-export async function action() {
-    return {
-        username: 'Nazwa użytkownika jest wymagana'
+export async function action({request}) {
+    const data = await request.formData();
+
+    const userToCreate = {
+        username: data.get('username'),
+        password: data.get('password'),
+        passwordRepeat: data.get('passwordRepeat'),
+        firstName: data.get('firstName'),
+        lastName: data.get('lastName')
     };
+
+    const response = await fetch(
+        'http://localhost:8080/api/auth/register',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userToCreate)
+        }
+    );
+
+    if (response.status !== 204)
+        return await response.json();
+
+    return redirect('/main/login?register-success');
 }
