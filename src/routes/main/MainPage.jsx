13a960a9c4166237e0f070766915897fc91421c8
Krzysztof <krzychuk1211@gmail.com>
2024-09-17 19:02:09 +0200

    Changing import. Reformatting the code.

--- src/routes/main/MainPage.jsx
+++ src/routes/main/MainPage.jsx
@@ -1,16 +1,16 @@
 import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 
-import MainNavigation from '../../components/main_page/MainNavigation';
-import classes from './MainPage.module.css';
+import MainNavigation from '../../components/main_page/navigation/MainNavigation';
+import './MainPage.module.css';
 
 function MainPage() {
     return (
         <>
-            <MainNavigation />
+            <MainNavigation/>
             <Container>
                 <main>
-                    <Outlet />
+                    <Outlet/>
                 </main>
             </Container>
         </>
