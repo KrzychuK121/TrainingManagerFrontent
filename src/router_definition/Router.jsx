d52d49a84f9b7e21634e0b0b2533dc766b88cc82
Krzysztof <krzychuk1211@gmail.com>
2024-09-18 17:51:57 +0200

    Moving router definition.

--- src/router_definition/Router.jsx
+++ src/router_definition/Router.jsx
@@ -1,15 +1,16 @@
 ﻿import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
-import Default from './routes/Default';
-import ExercisesDisplay, { loader as exercisesLoader } from './routes/entities/exercise/ExercisesDisplay';
-import MainPage from './routes/main/MainPage';
-import MainWelcome from './routes/main/MainWelcome';
-import LoginPage, { action as loginAction } from './routes/navigations/authentication/Login';
-import LogoutPage, { action as logoutAction } from './routes/navigations/authentication/Logout';
-import RegisterPage, { action as registerAction } from './routes/navigations/authentication/Register';
-import Welcome from './routes/Welcome';
-import { authenticatedLoader, isAuthenticated as isAuthLoader, nonAuthenticatedLoader } from './utils/AuthUtils';
+import Default from '../routes/Default';
+import MainPage from '../routes/main/MainPage';
+import MainWelcome from '../routes/main/MainWelcome';
+import LoginPage, { action as loginAction } from '../routes/navigations/authentication/Login';
+import LogoutPage, { action as logoutAction } from '../routes/navigations/authentication/Logout';
+import RegisterPage, { action as registerAction } from '../routes/navigations/authentication/Register';
+import Welcome from '../routes/Welcome';
+import { authenticatedLoader, isAuthenticated as isAuthLoader } from '../utils/AuthUtils';
+import AuthorizedPaths from './AuthorizedPaths';
 
+// TODO: add titles to pages https://dev.to/rohitnirban/adding-page-titles-to-react-app-23oe
 export const router = createBrowserRouter(
     [
         {
@@ -48,17 +49,7 @@ export const router = createBrowserRouter(
                             element: <LogoutPage/>,
                             action: logoutAction
                         },
-                        {
-                            path: '',
-                            loader: nonAuthenticatedLoader,
-                            children: [
-                                {
-                                    path: 'exercise',
-                                    element: <ExercisesDisplay/>,
-                                    loader: exercisesLoader
-                                }
-                            ]
-                        }
+                        AuthorizedPaths
                     ]
                 }
             ]
