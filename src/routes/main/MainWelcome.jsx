e2b51ffdea72d7d0ab4902f8fc5a5cc5e8fed5e7
Krzysztof <krzychuk1211@gmail.com>
2024-09-29 11:57:27 +0200

    Repairing condition and modifying alert.

--- src/routes/main/MainWelcome.jsx
+++ src/routes/main/MainWelcome.jsx
@@ -2,23 +2,18 @@ import './MainWelcome.module.css';
 import { useSearchParams } from 'react-router-dom';
 import AlertComponent from '../../components/alerts/AlertComponent';
 
-function loginSuccessPresent(loginSuccess) {
-    return loginSuccess !== null;
-}
-
 function MainWelcome() {
     // TODO: Maybe change content depending on auth status
     const [searchParams] = useSearchParams();
     const loginSuccess = searchParams.get('login-success');
+    const message = loginSuccess !== null
+        ? 'Logowanie ukończone pomyślnie.'
+        : '';
 
     return (
         <>
             <div>
-                <AlertComponent
-                    message='Logowanie ukończone pomyślnie.'
-                    displayCondition={loginSuccessPresent(loginSuccess)}
-                    autoClose={false}
-                />
+                <AlertComponent message={message} showTrigger={null}/>
                 <h1>Witaj w aplikacji!</h1>
                 <p style={{fontSize: '20px'}}>
                     Cieszę się, że zdecydowałeś/aś się skorzystać z aplikacji.
