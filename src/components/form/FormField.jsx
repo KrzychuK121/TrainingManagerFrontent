2dd8b5061433fc2c1bbf4f6e3a9f88f659db22a0
Krzysztof <krzychuk1211@gmail.com>
2024-09-29 12:04:07 +0200

    Changing logic so now the component requires input component. Exporting logic to `UseFormValidation`.

--- src/components/form/FormField.jsx
+++ src/components/form/FormField.jsx
@@ -1,39 +1,19 @@
 import { Form } from 'react-bootstrap';
 import classes from './FormField.module.css';
 
-function getValidationProp(isValid) {
-    if (isValid === undefined)
-        return {};
-    if (isValid)
-        return {isValid: true};
-    else
-        return {isInvalid: true};
-}
-
-function isValid(status) {
-    if (status === undefined)
-        return undefined;
-
-    return status === null;
-}
-
-function getValidationErrorMess(status) {
-    const isStatusValid = isValid(status);
-    if (isStatusValid === undefined)
+function getValidationErrorMess(errorMessages) {
+    if (errorMessages === undefined)
         return <></>;
 
-    let messages = isStatusValid
-        ? ''
-        : status;
-    let type = isStatusValid
-        ? 'valid'
-        : 'invalid';
+    let messages = errorMessages
+        ? errorMessages
+        : [''];
 
     return messages.map(
         message => (
             <Form.Control.Feedback
                 key={message}
-                type={type}
+                type='invalid'
             >
                 {message}
             </Form.Control.Feedback>
@@ -44,35 +24,19 @@ function getValidationErrorMess(status) {
 function FormField(
     {
         label,
-        status,
-        name,
-        type = 'text',
-        defaultValue = null,
+        errorMessages,
         children = undefined,
         helperText = null
     }
 ) {
-    const validationProps = getValidationProp(isValid(status));
 
     return (
         <Form.Group>
-            <Form.Label className='form-label' htmlFor={name} column={true}>{label}:</Form.Label>
-            {
-                children
-                    ? children
-                    : (
-                        <Form.Control
-                            className='form-control'
-                            id={name}
-                            name={name}
-                            type={type}
-                            defaultValue={defaultValue}
-                            isValid={validationProps.isValid}
-                            isInvalid={validationProps.isInvalid}
-                        />
-                    )
-            }
-            {getValidationErrorMess(status)}
+            <Form.Label className='d-block' column={true}>
+                {label}:
+                {children}
+                {getValidationErrorMess(errorMessages)}
+            </Form.Label>
             {
                 helperText && (
                     <div className={classes.helperBox}>
