a1dc6fd2bed3faa722b9821fbd597c9423123bce
Krzysztof <krzychuk1211@gmail.com>
2024-09-30 21:41:07 +0200

    Extending previous method that creates object out of formData retrieved in action. Creating filter method that removes elements if it does not contain provided keys.

--- src/utils/EntitiesUtils.js
+++ src/utils/EntitiesUtils.js
@@ -1,8 +1,42 @@
-export function createObjFromEntries(toCreate) {
+export function createObjFromEntries(toCreate, emptiesMapping = null) {
+    const requiredFunc = 'keys';
     if (!toCreate)
         throw new Error(`Object can't be null or undefined.`);
-    if ('entries' in toCreate && typeof toCreate.entries === 'function')
-        return Object.fromEntries(toCreate.entries());
-    else
-        throw new Error('Object have to contains entries() function.');
+    if (!(requiredFunc in toCreate) || typeof toCreate[requiredFunc] !== 'function')
+        throw new Error(`Object have to contains ${requiredFunc}() function.`);
+
+    const toReturn = {};
+
+    for (const key of toCreate.keys()) {
+        const allValues = toCreate.getAll(key);
+
+        if (allValues.length > 1)
+            toReturn[key] = allValues;
+        else {
+            if (
+                (!allValues[0] || !allValues[0].trim()) &&
+                emptiesMapping &&
+                emptiesMapping.hasOwnProperty(key)
+            )
+                toReturn[key] = emptiesMapping[key];
+            else
+                toReturn[key] = allValues[0];
+        }
+
+    }
+
+    return toReturn;
+}
+
+export function filterObject(toFilter, propsToFilter, remove = true) {
+    return Object.keys(toFilter)
+        .filter(
+            key => remove ?
+                !propsToFilter.includes(key)
+                : propsToFilter.includes(key)
+        )
+        .reduce((newObj, key) => {
+            newObj[key] = toFilter[key];
+            return newObj;
+        }, {});
 }
