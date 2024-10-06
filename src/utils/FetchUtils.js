c9ff163f1ca7846cb806f00ed7169f2ef127ab19
Krzysztof <krzychuk1211@gmail.com>
2024-10-06 18:38:19 +0200

    Defining function that will handle expired token scenario.

--- src/utils/FetchUtils.js
+++ src/utils/FetchUtils.js
@@ -1,8 +1,17 @@
-import { injectToken } from './AuthUtils';
+import { redirect } from 'react-router-dom';
+import { injectToken, logout, tokenExpired } from './AuthUtils';
 
 export function defaultHeaders(headers) {
     return injectToken({
         ...headers,
         'Content-Type': 'application/json'
     });
+}
+
+export async function handleResponseUnauthorized(response) {
+    if (tokenExpired(response)) {
+        const logoutResponse = await logout();
+        return redirect('/main/login');
+    } else
+        return null;
 }
