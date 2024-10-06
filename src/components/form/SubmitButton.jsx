43a6a6411787e66fe852aadccdb26e1b8021b1d5
Krzysztof <krzychuk1211@gmail.com>
2024-10-06 18:36:40 +0200

    Passing more props to buttons.

--- src/components/form/SubmitButton.jsx
+++ src/components/form/SubmitButton.jsx
@@ -1,12 +1,19 @@
 import { Button, Spinner } from 'react-bootstrap';
 import { useNavigation } from 'react-router-dom';
 
-function SubmitButton({display, submittingDisplay, variant = 'primary'}) {
+function SubmitButton(
+    {
+        display,
+        submittingDisplay,
+        variant = 'primary',
+        ...rest
+    }
+) {
     const navigation = useNavigation();
     return (
         navigation.state !== 'submitting'
-            ? <Button type='submit' variant={variant}>{display}</Button>
-            : <Button variant={variant} disabled>
+            ? <Button type='submit' variant={variant} {...rest}>{display}</Button>
+            : <Button variant={variant} disabled {...rest}>
                 {submittingDisplay}.. {' '}
                 <Spinner animation='grow' size='sm'/>
             </Button>
