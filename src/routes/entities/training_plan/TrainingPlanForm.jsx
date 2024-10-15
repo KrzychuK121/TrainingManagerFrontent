cd8a47e2e8533fe08ffad51266196f67d0dbfd97
Krzysztof <krzychuk1211@gmail.com>
2024-10-15 16:20:27 +0200

    Creating first implementation of save action.

--- src/routes/entities/training_plan/TrainingPlanForm.jsx
+++ src/routes/entities/training_plan/TrainingPlanForm.jsx
@@ -2,7 +2,8 @@ import { Col, Row } from 'react-bootstrap';
 import { Form as RouterForm, useLoaderData } from 'react-router-dom';
 import PlanDayAssign from '../../../components/entities/training_plan/PlanDayAssign';
 import SubmitButton from '../../../components/form/SubmitButton';
-import { sendDefaultParallelRequests } from '../../../utils/CRUDUtils';
+import { defaultHeaders, getIdPath, sendDefaultParallelRequests } from '../../../utils/CRUDUtils';
+import { createObjFromEntries } from '../../../utils/EntitiesUtils';
 
 function TrainingPlanForm({method = 'post'}) {
     const loadedData = useLoaderData();
@@ -11,10 +12,7 @@ function TrainingPlanForm({method = 'post'}) {
     return (
         <>
             {/*<AlertComponent />*/}
-            <RouterForm
-                method={method}
-                // th:object='${plansWrite}'
-            >
+            <RouterForm method={method}>
                 <Row className='justify-content-center'>
                     {
                         weekdays.map(
@@ -30,12 +28,13 @@ function TrainingPlanForm({method = 'post'}) {
                         )
                     }
                 </Row>
-                <Row className='justify-content-center'>
+                <div className='d-flex p-2 justify-content-center'>
                     <SubmitButton
                         display='Zapisz'
                         submittingDisplay='Zapisuję'
+                        className='d-inline-block'
                     />
-                </Row>
+                </div>
             </RouterForm>
         </>
     );
@@ -44,31 +43,54 @@ function TrainingPlanForm({method = 'post'}) {
 export default TrainingPlanForm;
 
 export async function loader() {
-    const [allTrainings, weekdays] = await sendDefaultParallelRequests(
+    const data = await sendDefaultParallelRequests(
         ['training/all', 'weekdays/read']
     );
 
-    return {
-        allTrainings,
-        weekdays
-    };
-    /*const [
-        trainingsResponse,
-        weekdaysResponse
-    ] = await Promise.all([
-        fetch('http://localhost:8080/api/training'),
-        fetch('http://localhost:8080/api/weekdays/read')
-    ]);
+    try {
+        const [allTrainings, weekdays] = data;
 
-    const [allTrainings, weekdays] = await Promise.all([
-        trainingsResponse.json(),
-        weekdaysResponse.json()
-    ]);
-
-    return json(
-        {
+        return {
             allTrainings,
             weekdays
+        };
+    } catch (ex) {
+        // if(ex instanceof TypeError) // Might be not useful
+        return data;
+    }
+}
+
+export async function action({request, params}) {
+    const trainingScheduleId = getIdPath(params);
+    const data = await request.formData();
+    const fromEntries = createObjFromEntries(data);
+
+    const transformedObj = {};
+
+    Object.keys(fromEntries).forEach(
+        formField => {
+            const [property, day] = formField.split('-');
+            if (!transformedObj[day])
+                transformedObj[day] = null;
+
+            if (fromEntries[formField]) {
+                if (transformedObj[day] === null)
+                    transformedObj[day] = {};
+                transformedObj[day][property] = fromEntries[formField];
+            }
         }
-    );*/
+    );
+
+    console.log(transformedObj);
+
+    const response = await fetch(
+        `http://localhost:8080/api/plans${trainingScheduleId}`,
+        {
+            method: request.method,
+            headers: defaultHeaders(),
+            body: JSON.stringify(transformedObj)
+        }
+    );
+
+    return null;
 }
