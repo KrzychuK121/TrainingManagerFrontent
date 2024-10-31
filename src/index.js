c2651e1ca74cc5da6248a11dbae64e714ec5d05b
Krzysztof <krzychuk1211@gmail.com>
2024-10-31 12:01:43 +0100

    Creating context responsible for websocket operations.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import WebsocketProvider from './contexts/ws/WebsocketProvider';
 import { router } from './router_definition/Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function injectTime() {
+    return `[${new Date().toISOString()}]: `;
+}
+
+console.log(`${injectTime()}invoked`);
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router}/>
+        <WebsocketProvider>
+            <RouterProvider router={router}/>
+        </WebsocketProvider>
     </React.StrictMode>
 );
