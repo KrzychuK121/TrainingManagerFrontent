99adb9dec386ad1111d97ce87993959d892397fd
KrzychuK121 <krzychuk1211@gmail.com>
2024-12-03 10:20:45 +0100

    Extracting BMI controls to separate file.

---
+++ src/components/calculators/BMIFields.jsx
@@ -0,0 +1,71 @@
+import FormControls from "./FormControls";
+import RadioButton from "./RadioButton";
+import {useState} from "react";
+
+export const SEXES = {
+    WOMEN: 'Kobieta',
+    MEN: 'Mężczyzna'
+};
+
+function BMIFields(
+    {
+        justifyContent = 'center'
+    }
+) {
+    const [chosenSex, setChosenSex] = useState(
+        {
+            sex: SEXES.WOMEN
+        }
+    );
+
+    return (
+        <>
+            <div
+                className={`d-flex justify-content-${justifyContent}`}
+            >
+                <RadioButton
+                    id='sexWomen'
+                    groupId='sex'
+                    label={SEXES.WOMEN}
+                    value={SEXES.WOMEN}
+                    chosenRadioValues={chosenSex}
+                    setChosenRadioValues={setChosenSex}
+                />
+                <RadioButton
+                    id='sexMen'
+                    groupId='sex'
+                    label={SEXES.MEN}
+                    value={SEXES.MEN}
+                    chosenRadioValues={chosenSex}
+                    setChosenRadioValues={setChosenSex}
+                />
+                <input
+                    type='hidden'
+                    name='sex'
+                    value={chosenSex.sex}
+                />
+            </div>
+
+            <FormControls
+                label='Wiek'
+                name='age'
+                justifyContent={justifyContent}
+                required
+            />
+            <FormControls
+                label='Wzrost [cm]'
+                name='height'
+                justifyContent={justifyContent}
+                required
+            />
+            <FormControls
+                label='Waga'
+                name='weight'
+                justifyContent={justifyContent}
+                required
+            />
+        </>
+    );
+}
+
+export default BMIFields;
