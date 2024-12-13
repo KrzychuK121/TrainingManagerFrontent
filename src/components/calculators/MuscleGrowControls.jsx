8993c51211b0f0f30976777ae1f48b416c74ca67
KrzychuK121 <krzychuk1211@gmail.com>
2024-12-13 14:52:50 +0100

    Mapping retrieved body parts form backend to controls components.

--- src/components/calculators/MuscleGrowControls.jsx
+++ src/components/calculators/MuscleGrowControls.jsx
@@ -1,8 +1,36 @@
-function MuscleGrowControls() {
+import BodyPartControls from "./BodyPartControls";
+import {useState} from "react";
+import {Row} from "react-bootstrap";
+
+function MuscleGrowControls(
+    {
+        bodyParts
+    }
+) {
+    const [checkedValues, setCheckedValues] = useState([]);
+
     return (
         <>
             <hr/>
             <h2>Trenowane części ciała</h2>
+            <p>
+                Wybierz części ciała, które chcesz wytrenować.
+            </p>
+            <Row>
+                {
+                    Object.entries(bodyParts).map(
+                        ([bodyPart, bodyPartDisplay]) => (
+                            <BodyPartControls
+                                key={bodyPart}
+                                bodyPart={bodyPart}
+                                bodyPartDisplay={bodyPartDisplay}
+                                setCheckedArr={setCheckedValues}
+                            />
+                        )
+                    )
+                }
+            </Row>
+            <hr/>
         </>
     );
 }
