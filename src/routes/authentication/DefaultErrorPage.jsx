76bad06f85d256f26046b15395ea50ae1c49431e
KrzychuK121 <krzychuk1211@gmail.com>
2025-01-17 13:16:59 +0100

    Working with styling in default error page.

--- src/routes/authentication/DefaultErrorPage.jsx
+++ src/routes/authentication/DefaultErrorPage.jsx
@@ -1,5 +1,7 @@
 import {useRouteError} from "react-router-dom";
 import classes from './DefaultErrorPage.module.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCircleExclamation} from "@fortawesome/free-solid-svg-icons";
 
 function DefaultErrorPage() {
     const routeError = useRouteError();
@@ -9,8 +11,13 @@ function DefaultErrorPage() {
             <div className={classes.errorHeader}>
                 <h1>Wystąpił błąd.</h1>
             </div>
-            <div className='d-flex justify-content-center align-items-center'>
+            <div className='d-flex my-5 justify-content-center align-items-center'>
                 <div className={classes.errorMessage}>
+                    <div className='d-inline-block' style={{marginRight: '20px'}}>
+                        <FontAwesomeIcon
+                            icon={faCircleExclamation}
+                        />
+                    </div>
                     <span>{routeError.message}</span>
                 </div>
             </div>
